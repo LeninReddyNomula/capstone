83cd02f3f9c61bf3ec90d6f49e4a433e82910b19
StarStarMe <103670046+StarStarMe@users.noreply.github.com>
2024-09-07 19:05:21 -0400

    Added comments again

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 // 2. Add your color mode config
 //added comments
+//added comments again
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
